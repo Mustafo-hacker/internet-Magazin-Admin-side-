af7ca8b9c471653c89f040f4226705e91c09c579
Mustafo-hacker <hafizov336@gmail.com>
2025-05-05 14:49:44 +0500

    second

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Dashbord from "./components/layouts/Dashboard/dashboard";
 import Dashboard from "/src/pages/Dashboard/dashboard";
 import Orders from "/src/pages/Orders/orders";
-import Products from "/src/pages/Products/products";
+import Products from "./pages/dashboard/dashboard";
 import Other from "/src/pages/Other/other";
 import Login from "./pages/login/Login";
 import Add from "./pages/add/Add";
