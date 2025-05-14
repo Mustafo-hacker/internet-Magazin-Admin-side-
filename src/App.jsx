ff01fbb4239ba8e34bc19799b856ac563769e84f
Mustafo-hacker <hafizov336@gmail.com>
2025-05-14 22:49:35 +0500

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Dashbord from "./components/layouts/Dashboard/dashboard";
-import Dashboard from "/src/pages/Dashboard/dashboard";
 import Orders from "/src/pages/Orders/orders";
 import Products from "./pages/dashboard/dashboard";
 import Other from "/src/pages/Other/other";
 import Login from "./pages/login/Login";
 import Add from "./pages/add/Add";
 import Edit from "./pages/edit/Edit";
+import Dashboard from "./pages/dashboard/dashboard";
 function App() {
   const router = createBrowserRouter([
     {
